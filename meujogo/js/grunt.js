e9fe4926285bbb9db6cd5e77e3f4c306786d36a6
ana- <ana23new@gmail.com>
2017-01-21 21:23:46 -0200

    Add files via upload

---
+++ meujogo/js/grunt.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+      'build/home.js': 'src/home.js',
+      'build/main.js': 'src/main.js'
+  });
+
+  // carrega plugins
+  grunt.loadNpmTasks('');
+};
